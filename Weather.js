21730c901e9eb24661c615cfa99e47daaeba96f3
YJC mac <yoon_je_chung@solomonsc.com>
2018-06-10 22:40:22 +0900

    added Ionicons from vector-icons

--- Weather.js
+++ Weather.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import{ LinearGradient } from "expo";
+import { LinearGradient } from "expo";
+import { Ionicons } from "@expo/vector-icons";
 
 export default class Weather extends Component {
     render() {
         return (
-        <LinearGradient 
-        colors = {["#00C6Fb","#005BEA"]} 
-        style = {styles.container}
-        >
-        <View style = {styles.upper}>
-            <Text>Icon here!</Text>
-            <Text style={styles.temp}>35</Text>
-        </View>
-        <View style={styles.lower}>
-            <Text style={styles.title}>Raining Like a MF</Text>
-            <Text style={styles.subtitle}>For more info look outside </Text>
-        </View>
-
+        <LinearGradient colors = {["#00C6Fb","#005BEA"]} style = {styles.container}>
+            <View style = {styles.upper}>
+                <Ionicons color= "white" size={144} name="ios-rainy" />
+                <Text style={styles.temp}>35</Text>
+            </View>
+            <View style={styles.lower}>
+                <Text style={styles.title}>Raining Like a MF</Text>
+                <Text style={styles.subtitle}>For more info look outside </Text>
+            </View>
         </LinearGradient>
         );
     }
@@ -30,10 +27,11 @@ const styles = StyleSheet.create({
   upper: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    backgroundColor: "transparent"
   },
   temp: {
-    fontSize: 38,
+    fontSize: 48,
     backgroundColor: "transparent",
     color: "white",
     marginTop: 10
