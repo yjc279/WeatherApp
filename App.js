35792975f9a705856c896c22a9a913cb6a9ff20d
YJC mac <yoon_je_chung@solomonsc.com>
2018-06-10 20:12:34 +0900

    created loading screen

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ Component } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    isLoaded: false
+
+  }
   render() {
+    const { isLoaded } = this.state.isLoaded;
     return (
       <View style={styles.container}>
-        <Text>Hello I am a Native developer now! And I love react!!</Text>
+        {isLoaded ? null : <View style={styles.loading}>
+          <Text style={styles.loadingText}>Getting the fucking weather</Text>
+        </View>}
       </View>
     );
   }
@@ -15,7 +22,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  loading: {
+    flex: 1,
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 100,
+  }
+
 });
